ad82886bcf4c08b0a396c70ea4c03357f0147df1
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-11-13 09:01:33 -0300

    feat(routes): add system routes

---
+++ front-end/src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { Home } from "../pages/home";
+import { Login } from "../pages/login";
+import { History } from "../pages/history";
+import { Profile } from "../pages/profile/Profile";
+import { Error } from "../pages/error";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+	return(
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Home />} path="/"/>
+				<Route element={<Login />} path="/login"/>
+				<Route element={<History />} path="/history"/>
+				<Route element={<Profile />} path="/profile"/>
+				<Route element={<Error />} path="/*"/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export { AppRoutes };
