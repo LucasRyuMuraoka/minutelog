2dfd8e2eba568d90e2b0d2609a8cb634dd643093
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-11-13 09:03:18 -0300

    feat(pages): add App.jsx file, to handle w. react

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import { AppRoutes } from "./routes/Routes"
+import { GlobalStyles } from "./assets/styles/global";
+
+const App = () => {
+  return (
+		<>
+			<AppRoutes />
+			<GlobalStyles />
+		</>
+  )
+}
+
+export { App };
